36fad7373b0beab1c7d13d09392c6ece8e71056e
kpc-simone <kpc.simone@gmail.com>
2023-11-05 11:36:02 -0500

    first commit

---
+++ examples/extract_phmtry_segments.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+from tkinter.filedialog import askdirectory,askopenfilename
+import pandas as pd
+import numpy as np
+import argparse
+import sys,os
+
+sys.path.insert(0,'../utils')
+from phmtry_tools import resample_signal, extract_segments
+
+### Script parameters ###
+SR_desired = 20
+
+pre_interval = -5
+segstart_colname = 'TPoI-2'
+event_dur = 8.
+post_interval = 12.
+
+baseline_pre_interval = -5
+baseline_end = 'TPoI-1'
+
+outcome_colname = 'observation'
+
+data_dir = askdirectory(title = 'Select folder containing processed photometry recordings, from which to extract all events. ')
+ec_file = askopenfilename(title = 'Select experimental conditions (ec) file, that defines the start and end times of the events. ')
+out_data_dir = askdirectory(title = 'Select directory to save the ouput data. ')
+
+phmtry_files = [f for f in os.listdir(data_dir) if '.csv' in f]
+
+outfilename = 'events-alignedto-{}-zscore.csv'.format(segstart_colname)
+ecdf = pd.read_csv(ec_file)
+
+ecdf = ecdf[ecdf[segstart_colname].notna()]
+
+ts_out = np.arange(start = pre_interval,stop = event_dur+post_interval,step = 1/SR_desired )
+out_df = pd.DataFrame( data = { 'time' : ts_out } )
+
+for phmtry_file in phmtry_files:
+
+    animal = phmtry_file.split('-')[0]
+    print(animal)
+    an_ecdf = ecdf[ecdf['animal'] == animal]
+    print(an_ecdf.head(10))
+    
+    if len(an_ecdf) > 0:
+        bl_times = [ [row[baseline_end]+baseline_pre_interval,row[baseline_end]] for r,row in an_ecdf.iterrows() ]
+        seg_times = [ [ row[segstart_colname]+pre_interval,row[segstart_colname]+event_dur+post_interval ] for r,row in an_ecdf.iterrows() ]
+        
+        labels = [ '{}-t{}-{}'.format(row['animal'],row['trial'],row[outcome_colname]) for r,row in an_ecdf.iterrows() ]
+        
+        phdf = pd.read_csv(os.path.join(data_dir,phmtry_file))
+        ts_res,ys_res = resample_signal(ts = phdf['time'], ys = phdf['fluo465-zsc'], SR_desired = SR_desired)
+                
+        '''
+        Probem: I need to generalize the z-score normalization procedure to have arbitrary baseline intervals
+        Solution: Pass a list of baseline interval times as a separate argument to extract_segments
+        '''
+
+        segs_df = extract_segments(ts = ts_res, 
+                                        ys = ys_res,
+                                        seg_times = seg_times,
+                                        labels = labels,
+                                        bl_times = bl_times,
+                                        )
+        
+        out_df = pd.concat( [out_df,segs_df], axis = 1 )
+        
+        # plot 1: verify segment extraction
+        fig,(ax1,ax2) = plt.subplots(1,2,figsize=(9,3),gridspec_kw={'width_ratios':[2,1]})
+        ax1.plot(ts_res,ys_res,color='dimgray')
+        for trial,(seg_start,seg_end) in zip(segs_df.columns,seg_times):
+            ax1.plot(out_df['time']+seg_start-pre_interval,out_df[ trial ])
+            ax1.axvline(seg_start,color='k',linestyle='--')
+            ax1.axvline(seg_end,color='k',linestyle='--')
+        
+        # plot 2: verify z-score normalization
+        ax2.plot(out_df['time'],out_df[segs_df.columns])
+        ax2.axvline(0.,color='k',linestyle='--')
+        ax2.axvline(event_dur,color='k',linestyle='--')
+        
+        for ax in (ax1,ax2):
+            ax.set_xlabel('Time (s)')
+            ax.set_ylabel('z-score')
+        
+        fig.suptitle(animal)
+        fig.tight_layout()
+        plt.show()
+
+out_df.to_csv(os.path.join(out_data_dir,outfilename))
+
+fig,ax = plt.subplots(1,1)
+
+zsc_mean = out_df.iloc[:,1:].mean(axis=1)
+zsc_std = out_df.iloc[:,1:].std(axis=1)
+ax.plot(out_df['time'],zsc_mean, color='tab:green')
+ax.fill_between(out_df['time'],zsc_mean-zsc_std,zsc_mean+zsc_std,color='tab:green',alpha=0.3)
+ax.set_xlabel('Time (s) from event onset')
+ax.set_ylabel('Z-Score')
+ax.axvline(0.,color='k',linestyle='--')
+ax.axvline(event_dur,color='k',linestyle='--')
+ax.set_xlim(pre_interval,event_dur+post_interval)
+plt.show()
---
+++ examples/process_doric_phmtry.py
@@ -0,0 +1,119 @@
+import matplotlib.pyplot as plt
+from tkinter.filedialog import askdirectory
+import pandas as pd
+import argparse
+import sys,os
+
+'''
+Processes raw photometry files given a directory. 
+Performs photobleaching correction, motion-artifact correction, and transformations to z-score. 
+
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseline_pre_start', type = float, nargs = '?', const = 100., default = 100.,
+                    help = 'Start of the pre-baseline interval for photobleaching correction, relative to the start of the recording. Default is t=100 seconds. ')
+parser.add_argument('--baseline_pre_end', type = float, nargs = '?', const = 600., default = 600.,
+                    help = 'End of the pre-baseline interval for photobleaching correction, relative to the start of the recording. Default is t=600 seconds. ')
+parser.add_argument('--baseline_post_start', type = float, nargs = '?', const = 500., default = 500.,
+                    help = 'Start of the pre-baseline interval for photobleaching correction, relative to the end of the recording. Default is t=500 seconds. ')
+parser.add_argument('--baseline_post_end', type = float, nargs = '?', const = 0., default = 0.,
+                    help = 'End of the pre-baseline interval for photobleaching correction, relative to the end of the recording. Default is t=0 seconds. ')
+
+sys.path.insert(0,'../utils')
+from phmtry_tools import load_phmtry_raw_doric, correct_photobleaching, correct_motion, transform_to_zscore
+
+# the organization of doric files can take multiple forms , depending on the particular version of doric studio installed
+# we need to homogenize the names of signals so that we can process them
+signals_map = {
+        'Console_time(s)'       : 'time',
+        'AIn-1 - Dem (AOut-1)'  : 'F-465',
+        'AIn-2 - Dem (AOut-2)'  : 'AF-405',
+        'DI--O-3'               : 'DI/03',
+        'DI--O-4'               : 'DI/04',
+        
+        'AIN01xAOUT01-LockIn/Time'        : 'time',
+        'AIN01xAOUT01-LockIn/Values'      : 'F-465',
+        'AIN02xAOUT02-LockIn/Values'      : 'AF-405',
+        'DigitalIO/DIO01'                 : 'DI/01',
+        'DigitalIO/DIO02'                 : 'DI/02',
+        'DigitalIO/DIO03'                 : 'DI/03',
+        }
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    baseline_pre_start = args.baseline_pre_start
+    baseline_pre_end = args.baseline_pre_end
+    baseline_post_start = args.baseline_post_start
+    baseline_post_end = args.baseline_post_end
+    plot = True
+
+    data_dir = askdirectory()
+    phmtry_files = [f for f in os.listdir(data_dir) if '.doric' in f]
+    
+    out_data_dir = os.path.join(data_dir,'../processed')
+    if not os.path.exists(out_data_dir):
+        os.mkdir(out_data_dir)
+    for phmtry_file in phmtry_files:
+
+        animal = phmtry_file.split('_')[0]
+        print(animal)
+
+        filepath = os.path.join(os.path.dirname(__file__),data_dir,phmtry_file)
+
+        # load the data from doric file
+        data = load_phmtry_raw_doric(filepath)
+        phdf = pd.concat([pd.DataFrame(v, columns=[k]) for k, v in data.items()], axis=1).rename( columns = signals_map )
+        phdf = phdf[['time','F-465','AF-405']].dropna()
+        # phdf = pd.DataFrame( data = data ).rename( columns = signals_map ).dropna()
+        print(phdf.head())
+
+        pre_interval_ = (baseline_pre_start,baseline_pre_end)
+        post_interval_ = ( phdf['time'].max()-baseline_post_start,phdf['time'].max()-baseline_post_end )
+        print(animal,pre_interval_,post_interval_)
+        
+        # correct for photobleaching
+        phdf['fluo465-pbc'] = correct_photobleaching( ts = phdf['time'], ys = phdf['F-465'],
+                                                            pre_interval = pre_interval_, 
+                                                            post_interval = post_interval_ 
+                                                            )
+        phdf['fluo405-pbc'] = correct_photobleaching( ts = phdf['time'], ys = phdf['AF-405'],
+                                                            pre_interval = pre_interval_,
+                                                            post_interval = post_interval_
+                                                            )
+        
+        # correct for motion-artifacts
+        phdf['fluo405-maf'], phdf['fluo465-mac'] = correct_motion(phdf['fluo465-pbc'].to_numpy(),phdf['fluo405-pbc'].to_numpy())
+        
+        # transform to z-score
+        phdf['fluo465-zsc'] = transform_to_zscore( ts = phdf['time'], 
+                                            ys = phdf['fluo465-mac'], 
+                                            baseline_interval = (120,480)
+                                            )
+
+        fig,(ax1,ax2,ax3,ax4) = plt.subplots(4,1,sharex=True,figsize=(6,10))
+        ax1.plot(phdf['time'],phdf['F-465'],label='465 channel')
+        ax1.plot(phdf['time'],phdf['AF-405'],label='405 channel')
+        ax1.axvspan(xmin=pre_interval_[0],xmax=pre_interval_[1],color='k',alpha=0.2)
+        ax1.axvspan(xmin=post_interval_[0],xmax=post_interval_[1],color='k',alpha=0.2)
+        ax1.set_title('Raw')
+        ax1.legend(loc='upper right')
+        
+        ax2.plot(phdf['time'],phdf['fluo465-pbc'],label='465 channel')
+        ax2.plot(phdf['time'],phdf['fluo405-pbc'],label='405 channel')
+        ax2.set_title('Photobleaching corrected')
+        ax2.legend(loc='lower right')
+
+        ax3.plot(phdf['time'],phdf['fluo465-pbc'],label='465 channel, before')
+        ax3.plot(phdf['time'],phdf['fluo405-maf'],label='405 channel, fit')
+        ax3.plot(phdf['time'],phdf['fluo465-mac'],label='465 channel, after')
+        ax3.set_title('Motion artifact correction')
+        ax3.legend(loc='lower right')
+        
+        ax4.plot(phdf['time'],phdf['fluo465-zsc'],label='465 channel')
+        ax4.set_title('Z-score normalization')
+        ax4.legend(loc='lower right')
+        fig.tight_layout()
+        plt.show()
+        
+        phdf.to_csv(os.path.join(out_data_dir,'{}-phmtry.csv'.format(animal)))
